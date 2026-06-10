78a3994541e177fab730d9e047cc1e09a447c7e6
Bhavesh Verma <bhaveshverma1234@gmail.com>
2026-06-10 21:11:38 +0530

    chore: allow localhost:8123 in server CORS for local static dev

--- server/index.js
+++ server/index.js
@@ -112,6 +112,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
+  'http://localhost:8123',
+  'http://127.0.0.1:8123',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
